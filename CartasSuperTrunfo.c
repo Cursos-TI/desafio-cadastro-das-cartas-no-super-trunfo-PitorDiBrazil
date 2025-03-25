e66ee8109ddc8b7b034304998518bb59dab97766
PitorDiBrazil <166571665+PitorDiBrazil@users.noreply.github.com>
2025-03-25 00:55:44 +0000

    Finalizei a segunda etapa do desafio

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,12 +11,10 @@ int main() {
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
 
-    // Dados de Identificação
+    //Variáveis Primeira Carta
 
     char Estado;
     char CodCidade[10];
-
-    // Nesta parte irei definir as váriaveis, no caso os atributos presentes em cada carta.
 
     char nome[50];
     int populacão;
@@ -24,15 +22,15 @@ int main() {
     float PIB;
     int  PT;
 
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+
+
+    // Introdução rápida sobre o jogo
     
    printf("Bem Vindo, ao cadastro de cartas do SuperTrunfo - Estados, para que tenhamos um jogo justo e decente, peço que prestem atenção no preenchimento dos dados das cartas. Dito isso, espero que tenha um bom jogo.\n");
    
    printf("Registro de Cartas do SuperTrunfo - Estados: Primeira Carta...\n");
 
-    //Depois de uma pequena introdução vou fazer a sequência de comandos que vão tratar do registro da carta.
+    //Registros da Primeira Carta
 
    printf("Insira a letra Identificadora do Estado dentre as disponiveis (de A a H):");
    scanf(" %c", &Estado);
@@ -58,40 +56,55 @@ int main() {
 /* Agr com todas as perguntas funcionando perfeitamente e o terminal funcionando de forma correta irei criar novamente variáveis novas para a segunda carta 
     e no final de ambos os registros será exibida a as duas cartas. */
 
-    // Antes de criar novas váriaveis vou testar para ver se é possível registrar a primeira carta exibir e depois registrar a segunda.
+    //Novas variáveis que representam a carta 2
 
-    printf("Aqui está sua primeira carta de código %s: \n", CodCidade);
-    printf("Estado: %c \n", Estado);
-    printf("Cidade: %s \n", nome);
-    printf("População: %i \n", populacão);
-    printf("Área KM²: %.3f km²\n", Área);
-    printf("PIB:R$ %.2f \n", PIB);
-    printf("Pontos Turísticos: %i", PT);
+    char Estado1;
+    char CodCidade1[10];
+
+    char nome1[50];
+    int populacão1;
+    float Área1;
+    float PIB1;
+    int  PT1;
     
     printf("Registro de Cartas do SuperTrunfo - Estados. Segunda Carta...\n");
 
-    //Depois de uma pequena introdução vou fazer a sequência de comandos que vão tratar do registro da segunda carta.
+    //Comandos de registro da segunda carta
 
    printf("Insira a letra Identificadora do Estado dentre as disponiveis (de A a H):");
-   scanf(" %c", &Estado);
+   scanf(" %c", &Estado1);
 
    printf("Insira o Código da Cidade (valores de 01 a 04):");
-   scanf("%s", CodCidade);
+   scanf("%s", CodCidade1);
 
    printf("Insira o nome da cidade:");
-   scanf("%s", nome);
+   scanf("%s", nome1);
 
    printf("Insira a população da cidade:");
-   scanf("%i", &populacão);
+   scanf("%i", &populacão1);
 
    printf("Insira a Área da cidade em KM²:");
-   scanf("%f", &Área);
+   scanf("%f", &Área1);
 
    printf("Insira o PIB da cidade:");
-   scanf("%f", &PIB);
+   scanf("%f", &PIB1);
 
    printf("Insira a quantidade de Pontos Turisticos presentes na cidade:");
-   scanf("%i", &PT);
+   scanf("%i", &PT1);
+   
+    // Variáveis calculadas Carta 1
+
+    float DensiPop = populacão / Área;
+    float PIBperC = PIB / populacão;
+
+    //variáveis calculadas carta 2
+    
+    float DensiPop1 = populacão1/Área1;
+    float PIBperC1 = PIB1/populacão1;
+
+   // Exibição das Cartas
+
+   // Primeira Carta
 
    printf("Aqui está sua primeira carta de código %s: \n", CodCidade);
    printf("Estado: %c \n", Estado);
@@ -99,11 +112,21 @@ int main() {
    printf("População: %i \n", populacão);
    printf("Área KM²: %.3f km² \n", Área);
    printf("PIB:R$ %.2f \n", PIB);
-   printf("Pontos Turísticos: %i", PT);
+   printf("Pontos Turísticos: %i \n", PT);
+   printf("Densidade Populacional: %.2f \n", DensiPop);
+   printf("PIB per Capita: %.2f \n", PIBperC);
 
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+   //Segunda Carta
+
+   printf("Aqui está sua segunda carta de código %s: \n", CodCidade1);
+   printf("Estado: %c \n", Estado1);
+   printf("Cidade: %s \n", nome1);
+   printf("População: %i \n", populacão1);
+   printf("Área KM²: %.3f km²\n", Área1);
+   printf("PIB:R$ %.2f \n", PIB1);
+   printf("Pontos Turísticos: %i \n", PT1);
+   printf("Densidade Populacional: %.2f \n", DensiPop1);
+   printf("PIB per Capita: %.2f \n", PIBperC1);;
 
     return 0;
 }
